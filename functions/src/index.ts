65853c487fe1c6a6c499b8189e671d3d37b631f7
Ondrej <ondrej.jarina@gmail.com>
2020-01-08 01:43:36 +0100

    [fix] Edited Firebase Cloud function
    Venues can be indexed and unindexed in Algolia

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,49 +22,11 @@ exports.indexVenue = functions.firestore
 		});
 	});
 
-async function updateDocumentInAlgolia(change: functions.Change<FirebaseFirestore.DocumentSnapshot>) {
-    const docBeforeChange = change.before.data()
-    const docAfterChange = change.after.data()
-    if (docBeforeChange && docAfterChange) {
-        if (docAfterChange.isIncomplete && !docBeforeChange.isIncomplete) {
-            // If the doc was COMPLETE and is now INCOMPLETE, it was 
-            // previously indexed in algolia and must now be removed.
-            await deleteDocumentFromAlgolia(change.after);
-        } else if (docAfterChange.isIncomplete === false) {
-            await saveDocumentInAlgolia(change.after);
-        }
-    }
-}
+exports.unindexVenue = functions.firestore
+  .document('venues/{venueId}')
+  .onDelete((snap, context) => {
+    const FirebaseObjectId = snap.id;
 
-async function saveDocumentInAlgolia(snapshot: any) {
-    if (snapshot.exists) {
-        const record = snapshot.data();
-        if (record) { // Removes the possibility of snapshot.data() being undefined.
-            if (record.isIncomplete === false) { // We only index products that are complete.
-                record.objectID = snapshot.id;
-                
-                // In this example, we are including all properties of the Firestore document 
-                // in the Algolia record, but do remember to evaluate if they are all necessary.
-                // More on that in Part 2, Step 2 above.
-                
-                await index.saveObject(record); // Adds or replaces a specific object.
-            }
-        }
-    }
-}
-
-async function deleteDocumentFromAlgolia(snapshot: FirebaseFirestore.DocumentSnapshot) {
-    if (snapshot.exists) {
-        const objectID = snapshot.id;
-        await index.deleteObject(objectID);
-    }
-	}
-
-export const collectionOnUpdate = functions.firestore.document('COLLECTION/{uid}').onUpdate(async (change, context) => {
-    await updateDocumentInAlgolia(change);
-});
-
-export const collectionOnDelete = functions.firestore.document('COLLECTION/{uid}').onDelete(async (snapshot, context) => {
-    await deleteDocumentFromAlgolia(snapshot);
-});
-
+    // Delete an ID from the index
+    return index.deleteObject(FirebaseObjectId);
+}); 
