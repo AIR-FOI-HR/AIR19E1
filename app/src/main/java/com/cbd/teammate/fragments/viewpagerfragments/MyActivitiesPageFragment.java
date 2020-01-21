5a881095c76f67d732064a9bab8297ef05c7d866
Pablo Martínez Figueroa <pablomf@hotmail.es>
2020-01-21 03:47:17 +0100

    List of My Activities (#17)
    Added a new view in which the user can see the the activities he/she's has created.

--- app/src/main/java/com/cbd/teammate/fragments/viewpagerfragments/MyActivitiesPageFragment.java
+++ app/src/main/java/com/cbd/teammate/fragments/viewpagerfragments/MyActivitiesPageFragment.java
@@ -3,13 +3,25 @@ package com.cbd.teammate.fragments.viewpagerfragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cbd.database.entities.Activity;
 import com.cbd.teammate.R;
+import com.cbd.teammate.holders.ActivityHolder;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +29,9 @@ import com.cbd.teammate.R;
 public class MyActivitiesPageFragment extends Fragment {
 
     private View view;
+    private RecyclerView activitiesList;
+    private FirestoreRecyclerAdapter<Activity, ActivityHolder> firestoreRecyclerAdapter;
+    private FirebaseFirestore db;
 
     public MyActivitiesPageFragment() {
         // Required empty public constructor
@@ -27,7 +42,93 @@ public class MyActivitiesPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_my_activities_page, container, false);
+
+        activitiesList = view.findViewById(R.id.venue_view_activities_mine);
+        activitiesList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        db = FirebaseFirestore.getInstance();
+
+        requestActivities();
+
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (firestoreRecyclerAdapter != null) {
+            firestoreRecyclerAdapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (firestoreRecyclerAdapter != null) {
+            firestoreRecyclerAdapter.stopListening();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (firestoreRecyclerAdapter != null) {
+            firestoreRecyclerAdapter.startListening();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (firestoreRecyclerAdapter != null) {
+            firestoreRecyclerAdapter.startListening();
+        }
+    }
+
+    private void requestActivities() {
+        String currentUser = Objects.requireNonNull(FirebaseAuth.getInstance().
+                getCurrentUser()).getUid();
+        db.collection("players")
+                .whereEqualTo("uid", currentUser).get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    String userId = queryDocumentSnapshots.iterator().next().getId();
+                    Query query = createQuery(userId);
+                    FirestoreRecyclerOptions<Activity> options = setOptions(query);
+                    setFirestoreRecyclerAdapter(options);
+                    activitiesList.setAdapter(firestoreRecyclerAdapter);
+                    if (firestoreRecyclerAdapter != null)
+                        firestoreRecyclerAdapter.startListening();
+                });
+    }
+
+    private void setFirestoreRecyclerAdapter(FirestoreRecyclerOptions<Activity> options) {
+        firestoreRecyclerAdapter
+                = new FirestoreRecyclerAdapter<Activity, ActivityHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ActivityHolder holder, int position, @NonNull Activity model) {
+                holder.setDetails(model);
+            }
+
+            @NonNull
+            @Override
+            public ActivityHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_layout_venue, parent, false);
+
+                return new ActivityHolder(view);
+            }
+        };
+    }
+
+    private Query createQuery(String userId) {
+        return db.collection("activities")
+                .whereEqualTo("creatorid", userId);
+    }
+
+    private FirestoreRecyclerOptions<Activity> setOptions(Query query) {
+        return new FirestoreRecyclerOptions.Builder<Activity>()
+                .setQuery(query, Activity.class)
+                .build();
     }
 
 }
